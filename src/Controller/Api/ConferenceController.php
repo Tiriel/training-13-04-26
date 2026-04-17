244938a3d6ccb36476d7d465d8e6ff451c135de6
Benjamin Zaslavsky <benjamin.zaslavsky@gmail.com>
2026-04-17 16:05:08 +0200

    API base endpoint

---
+++ src/Controller/Api/ConferenceController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controller\Api;
+
+use App\Search\ConferenceSearchInterface;
+use App\Search\Database\DatabaseConferenceSearch;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\DependencyInjection\Attribute\Autowire;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Serializer\Normalizer\AbstractNormalizer;
+
+final class ConferenceController extends AbstractController
+{
+    #[Route('/api/conference', name: 'app_api_conference')]
+    public function index(Request $request, #[Autowire(service: DatabaseConferenceSearch::class)] ConferenceSearchInterface $search): JsonResponse
+    {
+        $page = $request->query->get('page');
+
+        return $this->json($search->searchByName(page: $page), context: [
+            AbstractNormalizer::CIRCULAR_REFERENCE_HANDLER => fn($obj) => ['id' => $obj->getId()],
+        ]);
+    }
+}
